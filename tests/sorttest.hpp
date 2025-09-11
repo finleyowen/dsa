afa71e0650e9512a3f1e053cc4e59cc6862f1016
Finley AMO <finleyyowen@gmail.com>
2025-09-11 21:19:55 +1000

    Implemented merge sort.

--- tests/sorttest.hpp
+++ tests/sorttest.hpp
@@ -32,6 +32,20 @@ namespace SortTest
         }
     }
 
+    void test_non_inplace_sort(int *(*sortfn)(int[], size_t)) {
+        vector<int> x = get_test_data();
+        vector<int> y = get_sorted_test_data();
+
+        int *y_prime = sortfn(x.data(), x.size());
+
+        for (size_t i = 0; i < x.size(); i++)
+        {
+            assert(y_prime[i] == y[i]);
+        }
+
+        delete []y_prime;
+    }
+
     void test_selection_sort()
     {
         test_inplace_sort(selection_sort);
@@ -42,6 +56,12 @@ namespace SortTest
         test_inplace_sort(quick_sort);
     }
 
+    void test_merge_sort() 
+    {
+        test_non_inplace_sort(merge_sort);
+    }
+    
+
     void all()
     {
         cout << "Testing Selection Sort" << endl;
@@ -49,6 +69,9 @@ namespace SortTest
 
         cout << "Testing Quick Sort" << endl;
         test_quick_sort();
+
+        cout << "Testing Merge Sort" << endl;
+        test_merge_sort();
     }
 
 }
